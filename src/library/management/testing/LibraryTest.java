83afaa80c6e73586f9f6eabece8be4f13e8d728c
TheItsNamless <itsnameless@outlook.com>
2025-05-29 14:37:29 +0200

    Aufgabe 1

---
+++ src/library/management/testing/LibraryTest.java
@@ -0,0 +1,7 @@
+package library.management.testing;
+
+public class LibraryTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, Library Management System!");
+    }
+}
